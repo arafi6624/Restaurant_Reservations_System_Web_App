ef46e4c9859bf003ca69c165ee4d0511ac425023
Abdul Rafi <arafi6624@bths.edu>
2022-07-21 23:40:27 -0400

    CRUD Functionality completed, Some form validations completed, Bootstrap added

--- db/migrate/20220721224933_create_reservations.rb
+++ db/migrate/20220721224933_create_reservations.rb
@@ -1,11 +1,11 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-      t.string :name
-      t.integer :people
-      t.integer :tables
-      t.date :date
-      t.time :time
+      t.string :name, null:false
+      t.integer :people, null:false
+      t.integer :tables, null:false
+      t.date :date, null:false
+      t.time :time, null:false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_224933) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_22_001414) do
   create_table "reservations", force: :cascade do |t|
     t.string "name"
     t.integer "people"
     t.integer "tables"
     t.date "date"
-    t.time "time"
+    t.datetime "time", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
